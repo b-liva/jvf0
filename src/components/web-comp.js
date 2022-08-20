0d418639bfbe31ff62aa355bd041d3355c5c7d74
liva <ebammo20@gmail.com>
2022-08-21 01:06:48 +0430

    make apollo available

--- src/components/web-comp.js
+++ src/components/web-comp.js
@@ -1,6 +1,25 @@
+import {ApolloClient, createHttpLink, InMemoryCache} from "@apollo/client/core";
+import {provideApolloClient} from '@vue/apollo-composable'
+
 import {defineCustomElement} from 'vue'
 import MyWebComp from "./MyWebComp.ce.vue";
 import Mine from "./Mine.ce.vue";
+
+
+const httpLink = createHttpLink({
+    // You should use an absolute URL here
+    uri: 'https://countries.trevorblades.com/',
+})
+
+
+// Cache implementation
+const cache = new InMemoryCache()
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
+provideApolloClient((apolloClient));
+
 
 const MyWebElement = defineCustomElement(MyWebComp);
 const MineElement = defineCustomElement(Mine);
